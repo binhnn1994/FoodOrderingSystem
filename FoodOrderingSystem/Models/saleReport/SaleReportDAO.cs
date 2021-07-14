22befaabec13c96a93d85fd2bf73897917c7098c
datlt2001 <thanhdat3001@gmail.com>
2021-07-14 17:03:01 +0700

    sale Report

---
+++ FoodOrderingSystem/Models/saleReport/SaleReportDAO.cs
@@ -0,0 +1,17 @@
+﻿using FoodOrderingSystem.Utils;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.Models.saleReport
+{
+    public class SaleReportDAO : ISaleReportDAO
+    {
+        public IList<SaleReportObj> ListSaleReport(DateTime fromDate, DateTime toDate)
+        {
+            return null;
+        }
+    }
+}
---
+++ FoodOrderingSystem/Models/saleReport/SaleReportObj.cs
@@ -0,0 +1,17 @@
+﻿using FoodOrderingSystem.Models.customerOrder;
+using FoodOrderingSystem.Models.item;
+using FoodOrderingSystem.Models.orderDetails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.Models.saleReport
+{
+    public class SaleReportObj
+    {
+        public Item item { set; get; }
+        public CustomerOrder customerOrder { set; get; }
+        public OrderDetails orderDetails { set; get; }
+    }
+}
