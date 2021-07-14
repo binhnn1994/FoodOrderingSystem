6cec008e52f4e1126ac09087d555598c9ea10b42
datlt2001 <thanhdat3001@gmail.com>
2021-07-14 11:20:01 +0700

    fix merge

--- FoodOrderingSystem/Models/orderDetails/OrderDetailsDAO.cs
+++ FoodOrderingSystem/Models/orderDetails/OrderDetailsDAO.cs
@@ -32,5 +32,41 @@ namespace FoodOrderingSystem.Models.orderDetails
             }
             return result;
         }
+
+        public IList<OrderDetails> GetOrderDetails(string orderID)
+        {
+            IList<OrderDetails> orderDetails = new List<OrderDetails>();
+            try
+            {
+                using (var connection = new MySqlConnection(DBUtils.ConnectionString))
+                {
+                    connection.Open();
+                    string Sql = "Select orderID, itemID, quantity " +
+                            "From orderDetails " +
+                            "Where orderID = @orderID ";
+                    using (var command = new MySqlCommand(Sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@orderID", orderID);
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                orderDetails.Add(new OrderDetails
+                                {
+                                    OrderID = reader.GetString("orderID"),
+                                    ItemID = reader.GetString("itemID"),
+                                    Quantity = reader.GetInt32("quantity")
+                                });
+                            }
+                            return orderDetails;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
