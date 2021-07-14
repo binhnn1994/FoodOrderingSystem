47b96eec088131d1693ed12b9f1f31653d716bf6
huuquoc <huuquoc852@gmail.com>
2021-07-14 20:03:53 +0700

    Add StaffDashboard Page

---
+++ FoodOrderingSystem/Controllers/StaffDashboardController.cs
@@ -0,0 +1,21 @@
+﻿using FoodOrderingSystem.Models.account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.Controllers
+{
+    public class StaffDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
